a2a794b555e3ac57143f7341a5d544e8a0172c58
Ashif <114198892+Mhmd-Ashif@users.noreply.github.com>
2022-10-30 20:58:12 +0530

    initial drumkit files

---
+++ index.js
@@ -0,0 +1,75 @@
+var drumNumbers = document.querySelectorAll(".drum").length;
+
+//Works by clicking button via Mouse
+
+for (var i = 0; i < drumNumbers; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click" , function () {
+      
+      var drumButton = this.innerHTML;
+
+      makesound(drumButton);
+
+      buttonAnimation(drumButton);
+      });   
+}
+
+//Works by clicking buttons via Keyboard
+
+document.addEventListener("keypress", function(event){
+
+  makesound(event.key);
+
+  buttonAnimation(event.key);
+
+});
+
+//function that makes sound
+
+function makesound(key){
+  
+  switch (key) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "a":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;          
+    case "s":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break; 
+    case "d":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "j":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break; 
+    case "k":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break; 
+    case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;  
+    default:
+      console.log(drumButton);
+      break;
+  }
+}
+
+//Button Animation
+
+function buttonAnimation(currentkey){
+  var presentButton = document.querySelector("." + currentkey);
+  presentButton.classList.add("pressed");
+  setTimeout(function(){
+
+    presentButton.classList.remove("pressed");
+
+  }, 50);
+}
